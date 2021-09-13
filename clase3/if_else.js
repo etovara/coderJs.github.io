667b52e7e7ad772d5eee15e7853e62ebec1f79ed
Edwin <edwintovaraladejo@gmail.com>
2021-09-13 20:52:31 -0300

    Entregable 3

--- clase3/if_else.js
+++ clase3/if_else.js
@@ -88,12 +88,6 @@ while(false){
     console.log(acumulador)
 }
 
-//Ejemplo para entregable
-var tablaIngresada = prompt('Que tabla deseas ver:')
-var tablaNumero = parseInt(tablaIngresada)
 
-for(let i = 1; i <= 10; i++){
-    console.log ( i + ' * ' + tablaNumero + '=' + i * tablaNumero)
-}
 
 
